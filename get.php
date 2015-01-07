a72db784ccc49bc45b2527bd99a1bd3a10ad4a67
鈴木 將央 <henrymasaflight@yahoo.co.jp>
2015-01-07 21:31:49 +0900

    first commit

---
+++ get.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henrymasaflight
+ * Date: 2014/11/15
+ * Time: 16:09
+ */
+
+
+$cursor = '';
+
+$cont = array();
+
+$url = $_POST["url"];
+$id = call_user_func('end', preg_split('@/@', $url, -1, PREG_SPLIT_NO_EMPTY));
+
+
+$result = array(
+    'villege' => array(
+        'name' => '村人',
+        'win' => 0,
+        'all' => 0
+    ),
+    'knight' => array(
+        'name' => '騎士',
+        'win' => 0,
+        'all' => 0
+    ),
+    'madman' => array(
+        'name' => '狂人',
+        'win' => 0,
+        'all' => 0
+    ),
+    'uranai' => array(
+        'name' => '占い師',
+        'win' => 0,
+        'all' => 0
+    ),
+    'reinou' => array(
+        'name' => '霊能者',
+        'win' => 0,
+        'all' => 0
+    ),
+    'jinrou' => array(
+        'name' => '人狼',
+        'win' => 0,
+        'all' => 0
+    ),
+
+);
+
+function get_data($cursor = null,$id){
+
+    $url = 'http://www.werewolfonline.net/command/getuserplayedvillages';
+
+    $data = array(
+        'status' => '1',
+        'max' => '100',
+        'user_id' => $id,
+        'cursor' => $cursor
+
+    );
+    $headers = array(
+        'User-Agent: My User Agent 1.0',    //ユーザエージェントの指定
+        'Authorization: Basic '.base64_encode('user:pass'),//ベーシック認証
+        'Content-Type: application/x-www-form-urlencoded',
+        'Content-Length: '.strlen(http_build_query($data))
+    );
+    $options = array('http' => array(
+        'method' => 'POST',
+        'content' => http_build_query($data),
+        'header' => implode("\r\n", $headers),
+    ));
+    $contents = @file_get_contents($url, false, stream_context_create($options));
+
+    $contents = json_decode($contents, true);
+
+    return $contents;
+}
+
+$data = get_data($cursor,$id);
+$code = $data["returncode"];
+$cursor = $data["cursor"];
+
+
+if($code != 200){
+    echo json_encode("error");
+    
+}else{
+    
+    $cont = array_merge($cont,$data["villages"]);
+    
+    while($code == 200){
+    
+        $data = get_data($cursor,$id);
+        $code = $data["returncode"];
+        if($code == 200){
+            $cont = array_merge($cont,$data["villages"]);
+        }
+        $cursor = $data["cursor"];
+
+    }
+
+    foreach($cont as $value){
+
+        if($value["playerwin"] == 1){
+            $point = 1;
+        }else{
+            $point = 0;
+        };
+
+        if($value["playerrole"] == 1){
+            $result['villege']['all']++;
+            $result['villege']['win'] = $result['villege']['win'] + $point;
+
+        }
+
+        if($value["playerrole"] == 2){
+            $result['jinrou']['all']++;
+            $result['jinrou']['win'] = $result['jinrou']['win'] + $point;
+        }
+
+        if($value["playerrole"] == 3){
+            $result['uranai']['all']++;
+            $result['uranai']['win'] = $result['uranai']['win'] + $point;
+        }
+        if($value["playerrole"] == 4){
+            $result['madman']['all']++;
+            $result['madman']['win'] = $result['madman']['win'] + $point;
+        }
+        if($value["playerrole"] == 5){
+            $result['knight']['all']++;
+            $result['knight']['win'] = $result['knight']['win'] + $point;
+        }
+        if($value["playerrole"] == 6){
+            $result['reinou']['all']++;
+            $result['reinou']['win'] = $result['reinou']['win'] + $point;
+        }
+    }
+
+    $dataarr = array(
+        "all" => count($cont),
+        "detail" => ''
+        );
+
+    $dataarr["detail"] = $result;
+    $dataarr = json_encode($dataarr);
+
+
+    echo $dataarr;
+    
+}
+
+
+    
+    
+    
+
+
+
+
---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>人狼</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel='stylesheet' id='google_font-css'  href='http://fonts.googleapis.com/css?family=Roboto%3A500%2C900%2C100%2C300%2C700%2C400&#038;ver=4.1' type='text/css' media='all' />
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        
+        <div class="main_area">
+        	<h1>人狼オンラインX勝率計算ツール</h1>
+        	<div class="submit_area">
+        		<form method="post">
+				    <input id="value" type="text" name="url" placeholder="人狼オンラインXのユーザーページのURLを入力してください">
+				    <input id="button" type="submit" value="勝率を計算">
+				    
+				</form>
+        	</div>	
+        	
+			
+			<div id="result">
+                <span id="loading">
+                    <img src="img/loading.gif" height="55" width="54" alt="">
+                </span>
+			    	<table>
+                        <tr id="villege">
+                            <th>村人</th>
+                            <td class="game"><span>0</span>戦</td>
+                            <td class="win"><span>0</span>勝</td>
+                            <td class="percentage"><span>0</span>%</td>
+                        </tr>
+                        <tr id="knight">
+                            <th>騎士</th>
+                            <td class="game"><span>0</span>戦</td>
+                            <td class="win"><span>0</span>勝</td>
+                            <td class="percentage"><span>0</span>%</td>
+                        </tr>
+                        <tr id="madman">
+                            <th>狂人</th>
+                            <td class="game"><span>0</span>戦</td>
+                            <td class="win"><span>0</span>勝</td>
+                            <td class="percentage"><span>0</span>%</td>
+                        </tr>
+                        <tr id="uranai">
+                            <th>占い師</th>
+                            <td class="game"><span>0</span>戦</td>
+                            <td class="win"><span>0</span>勝</td>
+                            <td class="percentage"><span>0</span>%</td>
+                        </tr>
+                        <tr id="reinou">
+                            <th>霊能者</th>
+                            <td class="game"><span>0</span>戦</td>
+                            <td class="win"><span>0</span>勝</td>
+                            <td class="percentage"><span>0</span>%</td>
+                        </tr>
+                        <tr id="jinrou">
+                            <th>人狼</th>
+                            <td class="game"><span>0</span>戦</td>
+                            <td class="win"><span>0</span>勝</td>
+                            <td class="percentage"><span>0</span>%</td>
+                        </tr>
+                        <tr id="all">
+                            <th>総合</th>
+                            <td class="game"><span>0</span>戦</td>
+                            <td class="win"><span>0</span>勝</td>
+                            <td class="percentage"><span>0</span>%</td>
+                        </tr>
+			    		
+			    	</table>
+			    	
+			 </div>
+        	
+        </div>
+        
+        
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+
+
+        <script>
+    	
+        $(function(){
+            $("#button").click(function(event) {
+                $("#button").attr("disabled", "disabled");
+                $("#loading").fadeIn();
+                console.time('timer1');
+                
+            	var url = $("#value").val();
+            	$.ajax({
+            		url: "get.php",
+            		type: 'POST',
+            		data: {url: url},
+                    
+            	})
+            	.done(function() {
+            		console.log("success");
+                    
+                    
+            	})
+            	.fail(function() {
+            		console.log("error");
+                    alert("データを取得できませんでした");
+            	})
+            	.always(function(data) {
+            		console.log("complete");
+                    
+                    data = JSON.parse( data );
+                    if(data != "error"){
+                        
+                        
+                        var vildata = data.detail;
+                        console.log(vildata);
+                        var myObjKeys = Object.keys(vildata);
+                        
+                        var table = "";
+                        var allwin = 0;
+                        
+                        for (key in vildata) {
+                            $("#"+key+" .game span").text(vildata[key]['all']);
+                            $("#"+key+" .win span").text(vildata[key]['win']);
+                            $("#"+key+" .percentage span").text(keisan(vildata[key]['win']/vildata[key]['all']*100));
+                            
+                            allwin += vildata[key]['win'];
+                        };
+                        
+                        $("#all .game span").text(data.all);
+                        $("#all .win span").text(allwin);
+                        $("#all .percentage span").text(keisan(allwin/data.all*100));
+                        
+                        $("#loading").fadeOut();
+                        $('#button').removeAttr("disabled");
+                        
+                        console.timeEnd('timer1');
+                        
+                        // $(".percentage").each(function(index, el) {
+                        //     countup($("span",this),$(this).data("meta"),1);
+                        // });
+                        
+                        
+                        
+                        
+                        // for(var i=0; i<myObjKeys.length; i++){
+                            
+                        //     table += "<tr><th>"+vildata[myObjKeys[i]].name+"</th><td>"+vildata[myObjKeys[i]].all+"戦</td><td>"+vildata[myObjKeys[i]].win+"勝</td><td>勝率"+keisan(vildata[myObjKeys[i]].win/vildata[myObjKeys[i]].all*100)+"%</td><tr>"
+                        //     allwin += vildata[myObjKeys[i]].win;
+                        // };
+                        // table += "<tr><th>総合勝率</th><td>"+data.all+"戦</td><td>"+allwin+"勝</td><td>勝率"+keisan(allwin/data.all*100)+"%</td><tr>"
+                        
+                        
+                    }else{
+                        console.log("error");
+                        alert("データを取得できませんでした");
+                        $("#loading").fadeOut();
+                        $('#button').removeAttr("disabled");
+                    }
+            		
+                    
+            		
+            	});
+            	return false;
+            	
+            });
+            
+        });
+        
+        function keisan(num){
+
+			  
+			  var num = parseFloat(num);
+			  
+			  
+			  var num = num * 100;
+
+			  
+			  num = Math.round(num) / 100;
+			  
+			  return num;
+			  
+		}
+        function countup(tgt,max,speed){
+            var num = 0;
+            var ma = max*100
+            setInterval(function(){
+                if(num <= ma){
+                    tgt.html(num/100);
+                    num ++;
+                }
+            },speed);
+        }
+    </script>
+    </body>
+</html>
